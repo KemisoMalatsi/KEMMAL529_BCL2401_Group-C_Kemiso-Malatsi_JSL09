023b8a835d89cd44562d4da4c40289b53832c211
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-04-12 14:18:56 +0200

    Added a fetch request,promise chain and error handling with .catch method

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     })
 
    .catch(err => {
-
-
-
-
-
-
-
-
-
+        // Use a default background image and author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1420745981456-b95fe23f5753?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI5MjIxNzB8&ixlib=rb-4.0.3&q=80&w=1080)`
+
+        document.getElementById("author").textContent = `By: Dodi Achmad`
     })
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err))
