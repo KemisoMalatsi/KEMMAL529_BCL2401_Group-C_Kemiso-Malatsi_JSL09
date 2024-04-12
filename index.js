4b45e13e0d8a838dab4c542dde0af18f12234b44
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-04-12 13:59:01 +0200

    Added the .catchmethod to handle the errors

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
+        console.log(data.urls.regular)
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+
+   .catch(err => {
+
+
+
+
+
+
+
+
+
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1420745981456-b95fe23f5753?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI5MjIxNzB8&ixlib=rb-4.0.3&q=80&w=1080)`
     })
