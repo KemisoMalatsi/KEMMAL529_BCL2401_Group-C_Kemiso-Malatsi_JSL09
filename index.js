4580607e5a4595e377f377f7ff26e4f0a65aa280
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-04-12 10:20:38 +0200

    sets the background image of the webpage body to the full-size image URL fetched from the API

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+    })
